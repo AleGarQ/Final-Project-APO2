cad733bc83c74faf0dbe8bab522a8133387a5a34
IsaacEscobar <isak.escobarr@gmail.com>
2019-11-05 15:19:07 -0500

    otroxd

---
+++ src/model/User.java
@@ -0,0 +1,76 @@
+package model;
+
+public class User {
+
+	private String name;
+	private String id;
+	private String password;
+	private String email;
+	private int age;
+	private String phoneNumber;
+	
+	private User left;
+	private User right;
+	private ReservedRoom rRooms;
+	private FavoriteRoom fRooms;
+	private CustomList customList;
+	private SearchHistory record;
+	
+	public User(String name, String id, String password, String email, int age, String phoneNumber) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		this.email = email;
+		this.age = age;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}
