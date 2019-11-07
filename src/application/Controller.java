fa1651014547b5253ccbad5c993c298e85140c7c
Alejandro Garcia <alejo.gar.122@gmail.com>
2019-11-07 16:47:36 -0500

    Chisporroteo jiji

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,9 +1,46 @@
 package application;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import model.*;
 
-public class Controller {
-
+public class Controller implements Initializable{
 	private User users;
 	private Hotel hotels;
+	@FXML Pane firstScreen = new AnchorPane();
+	@FXML Button signIn = new Button();
+	@FXML Button signUp = new Button();
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+	
+	}
+	
+	public void signInScreen() {
+		firstScreen.getChildren().clear();
+		VBox vb = new VBox(3);
+		Image img = new Image("/resources/Logo.jpeg");
+		ImageView logoIMG = new ImageView(img);
+		logoIMG.setFitHeight(150);
+		logoIMG.setFitWidth(150);
+		TextField tf = new TextField("Nombre de usuario");
+		TextField tf2 = new TextField("Contraseña");
+		vb.getChildren().add(0, logoIMG);
+		vb.getChildren().add(1, tf);
+		vb.getChildren().add(2, tf2);
+		firstScreen.getChildren().add(vb);
+		firstScreen.setMaxSize(vb.getWidth(), vb.getHeight());
+	}
 	
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,20 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Window.fxml"));
+			root.getStylesheets().add(getClass().getResource("styleFirstScreen.css").toExternalForm());
+			primaryStage.setTitle("Inicio");
+            primaryStage.setScene(new Scene(root));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
