d7c93fa2030717b9caf14075d3e51ef932a633c9
Alejandro Garcia <alejo.gar.122@gmail.com>
2019-11-08 13:21:18 -0500

    El izak si jode mano

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -34,13 +36,45 @@ public class Controller implements Initializable{
 		ImageView logoIMG = new ImageView(img);
 		logoIMG.setFitHeight(150);
 		logoIMG.setFitWidth(150);
-		TextField tf = new TextField("Nombre de usuario");
-		TextField tf2 = new TextField("Contraseña");
+		TextField tf = new TextField();
+		TextField tf2 = new TextField();
+		tf.setPromptText("Correo electronico");
+		tf2.setPromptText("Contraseña");
 		vb.getChildren().add(0, logoIMG);
 		vb.getChildren().add(1, tf);
 		vb.getChildren().add(2, tf2);
+		vb.setMinSize(firstScreen.getWidth(), firstScreen.getHeight());
 		firstScreen.getChildren().add(vb);
-		firstScreen.setMaxSize(vb.getWidth(), vb.getHeight());
+	}
+	
+	public void signUpScreen() {
+		firstScreen.getChildren().clear();
+		ScrollPane sp = new ScrollPane();
+		VBox vb = new VBox(7);
+		Image img = new Image("/resources/Logo.jpeg");
+		ImageView logoIMG = new ImageView(img);
+		logoIMG.setFitHeight(70);
+		logoIMG.setFitWidth(70);
+		TextField fullName = new TextField();
+		TextField id = new TextField();
+		TextField age = new TextField();
+		TextField phone = new TextField();
+		TextField email = new TextField();
+		PasswordField password = new PasswordField();
+		fullName.setPromptText("Nombres y apellidos");
+		id.setPromptText("Numero de identificación");
+		age.setPromptText("Fecha de nacimiento DD/MM/AAAA");
+		phone.setPromptText("Numero de telefono");
+		email.setPromptText("Correo electronico");
+		password.setPromptText("Contraseña");
+		for(int i=1; i<vb.getChildren().size(); i++) {
+			
+		}
+		vb.getChildren().add(0, logoIMG);
+//		vb.getChildren().add(1, tf);
+//		vb.getChildren().add(2, tf2);
+		vb.setMinSize(firstScreen.getWidth(), firstScreen.getHeight());
+		firstScreen.getChildren().add(vb);
 	}
 	
 }
