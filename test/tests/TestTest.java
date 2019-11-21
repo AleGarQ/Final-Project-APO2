df90398ef8497c8faae5b56d72622266a5bc7837
IsaacEscobar <isak.escobarr@gmail.com>
2019-11-20 23:01:21 -0500

    me da error pero no se por que xd

--- test/tests/TestTest.java
+++ test/tests/TestTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import exceptions.ExistentException;
+import exceptions.WrongInformation;
 import model.*;
 
 class TestTest {
@@ -13,14 +13,16 @@ class TestTest {
 	
 	public void init() {
 		User userTest = new User("Isaac", "1006206201", "contraseña", "isak@gmail.com", "02/12/2000", "3564025", null, null);
+		p.setUsers(userTest);
 	}
 
 	@Test
-	void signInTest() {
+	void signInTest() throws WrongInformation {
 		init();
+		User user = new User("Isaac", "1006206201", "contraseña", "isak@gmail.com", "02/12/2000", "3564025", null, null);
 		String psw = "contraseña";
 		String email = "isak@gmail.com";
-		assertEquals();
+		assertEquals(user, p.signInFinal(email, psw));
 		}
 
 }
